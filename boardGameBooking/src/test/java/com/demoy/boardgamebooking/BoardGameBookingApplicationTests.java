177aa459c6b42d3f253f4fb4e8d09533f746f9ef
demoy <demoy16.alex@mail.ru>
2025-10-21 17:27:11 +0300

    init project

---
+++ boardGameBooking/src/test/java/com/demoy/boardgamebooking/BoardGameBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoy.boardgamebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
